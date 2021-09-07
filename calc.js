92d42ee55963b01f655267e0f8a50485e810c6da
sriram000 <srirammani14@gmail.com>
2021-09-07 11:27:53 +0000

    Initialized.

---
+++ calc.js
@@ -0,0 +1,9 @@
+const operations = {
+    add: (a, b) => a + b,
+    sub: (a, b) => a - b,
+    mul: (a, b) => a * b,
+    div: (a, b) => a / b,
+    mod: (a, b) => a % b,
+};
+
+export default operations;
